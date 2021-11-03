dfb3b8e3110a183900c3290c88960025220ff6b8
drvarun <12458730+drvarun@users.noreply.github.com>
2021-11-03 17:50:26 +0100

    aes 256

--- src/getPublicKey.c
+++ src/getPublicKey.c
@@ -18,52 +18,69 @@ void handleGetPublicKey(uint8_t p1, uint8_t p2, uint8_t *dataBuffer, uint16_t da
 		uint8_t buffer[32] = {0};
 		retcode = cx_get_random_bytes(buffer, 32);
 	*/
+
 	uint32_t bip32Path[] = {44 | 0x80000000, 93 | 0x80000000, 1 | 0x80000000, 0x80000000, 0x80000000};
 	uint8_t keySeed[32];
-	cx_ecfp_private_key_t privateKey;
-	cx_ecfp_public_key_t publicKey;
+	// cx_ecfp_private_key_t privateKey;
+	// cx_ecfp_public_key_t publicKey;
+	cx_aes_key_t aesKey;
 
 	// Derive Seed1
 	os_perso_derive_node_bip32(CX_CURVE_256K1, bip32Path, 5, keySeed, NULL);
 	PRINTF("Seed1:\n %.*h \n", 32, keySeed);
 
-	// Initiate the private key with the seed1
-	cx_ecfp_init_private_key(CX_CURVE_256K1, keySeed, 32, &privateKey);
+	cx_aes_init_key(keySeed, 32, &aesKey);
+	unsigned char plaintext[] = "ARCHETHIC";
+	unsigned char cipher[50] = {0};
+	unsigned char recoveredtext[50] = {0};
 
-	// Generate the corresponding public key1
-	cx_ecfp_generate_pair(CX_CURVE_256K1, &publicKey, &privateKey, 1);
-	PRINTF("Pub Key1:\n %.*h \n", publicKey.W_len, publicKey.W);
+	PRINTF("Plain:\n %.*h \n", sizeof(plaintext), plaintext);
+	int cipher_size = cx_aes(&aesKey, CX_LAST | CX_ENCRYPT | CX_CHAIN_CBC | CX_PAD_ISO9797M2, plaintext, sizeof(plaintext), cipher, sizeof(cipher));
+	PRINTF("Cipher:\n %.*h \n", cipher_size, cipher);
 
-	uint32_t bip32Path2[] = {44 | 0x80000000, 93 | 0x80000000, 2 | 0x80000000, 0x80000000, 0x80000000};
-	uint8_t keySeed2[32];
-	cx_ecfp_private_key_t privateKey2;
-	cx_ecfp_public_key_t publicKey2;
+	int plain_size = cx_aes(&aesKey, CX_DECRYPT | CX_CHAIN_CBC | CX_PAD_ISO9797M2, cipher, cipher_size, recoveredtext, sizeof(recoveredtext));
+	PRINTF("Recovered:\n %.*h \n", plain_size, recoveredtext);
 
-	// Derive Seed2
-	os_perso_derive_node_bip32(CX_CURVE_256K1, bip32Path2, 5, keySeed2, NULL);
-	PRINTF("Seed2:\n %.*h \n", 32, keySeed2);
+	io_exchange_with_code(SW_OK, 0);
+	/*
+		// Initiate the private key with the seed1
+		cx_ecfp_init_private_key(CX_CURVE_256K1, keySeed, 32, &privateKey);
 
-	// Initiate the private key2 with the seed2
-	cx_ecfp_init_private_key(CX_CURVE_256K1, keySeed2, 32, &privateKey2);
+		// Generate the corresponding public key1
+		cx_ecfp_generate_pair(CX_CURVE_256K1, &publicKey, &privateKey, 1);
+		PRINTF("Pub Key1:\n %.*h \n", publicKey.W_len, publicKey.W);
 
-	// Generate the corresponding public key2
-	cx_ecfp_generate_pair(CX_CURVE_256K1, &publicKey2, &privateKey2, 1);
-	PRINTF("Pub Key2:\n %.*h \n", publicKey2.W_len, publicKey2.W);
+		uint32_t bip32Path2[] = {44 | 0x80000000, 93 | 0x80000000, 2 | 0x80000000, 0x80000000, 0x80000000};
+		uint8_t keySeed2[32];
+		cx_ecfp_private_key_t privateKey2;
+		cx_ecfp_public_key_t publicKey2;
 
-	unsigned char ecdh1[65] = {0};
-	unsigned char ecdh2[65] = {0};
+		// Derive Seed2
+		os_perso_derive_node_bip32(CX_CURVE_256K1, bip32Path2, 5, keySeed2, NULL);
+		PRINTF("Seed2:\n %.*h \n", 32, keySeed2);
 
-	int ecdh1_size = cx_ecdh(&privateKey, CX_ECDH_POINT, publicKey2.W, publicKey2.W_len, ecdh1, 65);
-	for (int v = 0; v < ecdh1_size; v++)
-	{
-		G_io_apdu_buffer[v] = ecdh1[v];
-	}
+		// Initiate the private key2 with the seed2
+		cx_ecfp_init_private_key(CX_CURVE_256K1, keySeed2, 32, &privateKey2);
 
-	int ecdh2_size = cx_ecdh(&privateKey2, CX_ECDH_POINT, publicKey.W, publicKey.W_len, ecdh2, 65);
+		// Generate the corresponding public key2
+		cx_ecfp_generate_pair(CX_CURVE_256K1, &publicKey2, &privateKey2, 1);
+		PRINTF("Pub Key2:\n %.*h \n", publicKey2.W_len, publicKey2.W);
 
-	for (int v = 0; v < ecdh2_size; v++)
-	{
-		G_io_apdu_buffer[ecdh1_size + v] = ecdh2[v];
-	}
-	io_exchange_with_code(SW_OK, ecdh1_size + ecdh2_size);
+		unsigned char ecdh1[65] = {0};
+		unsigned char ecdh2[65] = {0};
+
+		int ecdh1_size = cx_ecdh(&privateKey, CX_ECDH_POINT, publicKey2.W, publicKey2.W_len, ecdh1, 65);
+		for (int v = 0; v < ecdh1_size; v++)
+		{
+			G_io_apdu_buffer[v] = ecdh1[v];
+		}
+
+		int ecdh2_size = cx_ecdh(&privateKey2, CX_ECDH_POINT, publicKey.W, publicKey.W_len, ecdh2, 65);
+
+		for (int v = 0; v < ecdh2_size; v++)
+		{
+			G_io_apdu_buffer[ecdh1_size + v] = ecdh2[v];
+		}
+
+		*/
 }
