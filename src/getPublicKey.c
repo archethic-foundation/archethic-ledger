bf49e8e9670d16e545e828a230a15865328d6d13
drvarun <12458730+drvarun@users.noreply.github.com>
2021-11-05 18:27:47 +0100

    ecdsa

--- src/getPublicKey.c
+++ src/getPublicKey.c
@@ -1,3 +1,8 @@
+// elixir verify
+// public_key = Base.decode16!("", case: :lower)
+// sign = Base.decode16!("", case: :lower)
+// :crypto.verify(:ecdsa, :sha256, "archethic", sign, [public_key, :secp256k1])
+
 #include <stdint.h>
 #include <stdbool.h>
 #include <os.h>
@@ -21,8 +26,24 @@ void handleGetPublicKey(uint8_t p1, uint8_t p2, uint8_t *dataBuffer, uint16_t da
 
 	uint32_t bip32Path[] = {44 | 0x80000000, 93 | 0x80000000, 1 | 0x80000000, 0x80000000, 0x80000000};
 	uint8_t keySeed[32];
-	// cx_ecfp_private_key_t privateKey;
-	// cx_ecfp_public_key_t publicKey;
+	cx_ecfp_private_key_t privateKey;
+	cx_ecfp_public_key_t publicKey;
+	uint8_t signature[100];
+	unsigned int info = 0;
+	uint8_t hash_archethic[32] = {0xa7, 0xf9, 0x84, 0xd0, 0x0e, 0x81, 0xd1, 0x26, 0x99, 0x71, 0x9f, 0xff, 0xc1, 0xca, 0xbd, 0x14, 0x93, 0x7a, 0xda, 0x86, 0x7d, 0x4b, 0x85, 0xdc, 0x25, 0xfe, 0xf3, 0xbf, 0xab, 0x0a, 0x95, 0x0a};
+
+	os_perso_derive_node_bip32(CX_CURVE_256K1, bip32Path, 5, keySeed, NULL);
+	PRINTF("Priv Key:\n %.*h \n", 32, keySeed);
+	cx_ecfp_init_private_key(CX_CURVE_256K1, keySeed, 32, &privateKey);
+
+	// Generate the corresponding public key
+	cx_ecfp_generate_pair(CX_CURVE_256K1, &publicKey, &privateKey, 1);
+	PRINTF("Pub Key:\n %.*h \n", publicKey.W_len, publicKey.W);
+
+	int sig_size = cx_ecdsa_sign(&privateKey, CX_RND_RFC6979, CX_SHA256, hash_archethic, 32, signature, sizeof(signature), &info);
+	PRINTF("Sig:\n %.*h \n", sig_size, signature);
+	io_exchange_with_code(SW_OK, 0);
+	/*
 	cx_aes_key_t aesKey;
 
 	// Derive Seed1
@@ -40,9 +61,10 @@ void handleGetPublicKey(uint8_t p1, uint8_t p2, uint8_t *dataBuffer, uint16_t da
 
 	int plain_size = cx_aes(&aesKey, CX_DECRYPT | CX_CHAIN_CBC | CX_PAD_ISO9797M2, cipher, cipher_size, recoveredtext, sizeof(recoveredtext));
 	PRINTF("Recovered:\n %.*h \n", plain_size, recoveredtext);
-
-	io_exchange_with_code(SW_OK, 0);
+	*/
 	/*
+	cx_ecfp_public_key_t publicKey;
+
 		// Initiate the private key with the seed1
 		cx_ecfp_init_private_key(CX_CURVE_256K1, keySeed, 32, &privateKey);
 
