3867e716c0ec6d7ee8ac15c6d050e17b5cccdbb2
Prince Anuragi <prince@uniris.io>
2022-01-19 14:49:21 +0530

    Added Testing for Ledger Application

---
+++ tests/test_name_version.py
@@ -0,0 +1,10 @@
+def test_get_app_and_version(cmd, hid):
+    if hid:
+        # for now it doesn't work with Speculos
+        app_name, version = cmd.get_app_and_version()
+
+        print(app_name)
+        print(version)
+
+        assert app_name == "ARCHEthic"
+        assert version == "1.0.1"
